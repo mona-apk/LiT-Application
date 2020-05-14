2e00efcebccc71602032f7326d665e4cbde97a5a
Mikami <cordion_nap@yahoo.co.jp>
2020-05-15 02:48:44 +0900

    Profileアプリ:応用・チャレンジ

--- profile/src/main/java/com/kamiapk/profile/MainActivity.kt
+++ profile/src/main/java/com/kamiapk/profile/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.kamiapk.profile
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,12 +12,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        profileImage.isVisible = false
-        profileCommentText.isVisible = false
-
         button1.setOnClickListener {
-            profileImage.isVisible = true
-            profileCommentText.isVisible = true
+            profileLabelText.text = "名前"
+            profileImage.setImageResource(R.drawable.randy_image)
+            profileCommentText.text = "Androidメンターのランディだよ"
+
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(220,0,100))
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+        }
+        button2.setOnClickListener {
+            profileLabelText.text = "スポーツ"
+            profileImage.setImageResource(R.drawable.baseball_image)
+            profileCommentText.text = "野球観戦が好きで、スタジアムに通っている"
+
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(230,230,0))
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+        }
+        button3.setOnClickListener {
+            profileLabelText.text = "好きな食べ物"
+            profileImage.setImageResource(R.drawable.donut_image)
+            profileCommentText.text = "キャンディやドーナツ"
+
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(255,102,255))
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+        }
+        button4.setOnClickListener {
+            profileLabelText.text = "趣味"
+            profileImage.setImageResource(R.drawable.gadget_image)
+            profileCommentText.text = "ガジェットを集めて動かすこと"
+
+            button4.backgroundTintList = ColorStateList.valueOf(Color.rgb(0,180,120))
+            button1.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button2.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
+            button3.backgroundTintList = ColorStateList.valueOf(Color.rgb(150,150,150))
         }
     }
 }
