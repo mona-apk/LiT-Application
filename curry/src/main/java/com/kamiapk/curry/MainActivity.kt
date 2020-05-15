a8ea5bec3d8cfd70f361b6e4708f071086b8fa6a
Mikami <cordion_nap@yahoo.co.jp>
2020-05-15 18:57:10 +0900

    カレーよそう: アプリ完成

--- curry/src/main/java/com/kamiapk/curry/MainActivity.kt
+++ curry/src/main/java/com/kamiapk/curry/MainActivity.kt
@@ -2,11 +2,38 @@ package com.kamiapk.curry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val animals = arrayOf("いのしし","ごりら","にわとり","きつね")
+    private val foods = arrayOf("カレー","ナンカレー","インドカレー","チキンカレー")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            game()
+        }
+    }
+
+    private fun game() {
+        val animalIndex1 = Random.nextInt(animals.size)
+        var animalIndex2 = Random.nextInt(animals.size)
+        val foodIndex = Random.nextInt(foods.size)
+
+        while (animalIndex1 == animalIndex2) {
+            animalIndex2 = Random.nextInt(animals.size)
+        }
+
+        textView.text = "${animals[animalIndex1]}は"
+        textView2.text = "${animals[animalIndex2]}に"
+        val result = if( (Random.nextInt(100) + 1) >= 50) {
+            "をよそえました!"
+        } else {
+            "をよそえませんでした"
+        }
+        textView3.text = "${foods[foodIndex]}を$result"
     }
 }
